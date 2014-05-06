630f231a0f9da548a416e7834283c94177b201ba
Corey Norris <coreypnorris@gmail.com>
2014-05-06 16:25:30 -0700

    create contactcontroller to handle delete actions

---
+++ app/assets/javascripts/controllers/contact_controller.js
@@ -0,0 +1,9 @@
+App.ContactController = Ember.ObjectController.extend({
+  actions: {
+    deleteContact: function() {
+      if (confirm('Are you sure?')) {
+        this.get('model').destroyRecord();
+      }
+    }
+  }
+});
