8684c7fba5c719ca4b4046ed2b6bf733ff5e887c
Corey Norris <coreypnorris@gmail.com>
2014-05-06 15:18:39 -0700

    create new_contact_controller

---
+++ app/assets/javascripts/controllers/new_contact_controller.js
@@ -0,0 +1,16 @@
+App.NewContactController = Ember.ObjectController.extend({
+  actions: {
+    createContact: function() {
+      var model = this.get('model');
+      var controller = this;
+
+      model.save();
+      .then(function() {
+        controller.transitionToRoute('contact', model);
+      });
+      .catch(function() {
+        alert("Please fix the problems noted.");
+      });
+    }
+  }
+});
